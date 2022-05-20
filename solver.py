4aa9c145760a45a54ef190aa95140bab13e6cf28
Ryan Sandridge <noreply@ryansandridge.com>
2022-05-20 18:42:43 -0400

    Minor refactor of solver into a class

--- solver.py
+++ solver.py
@@ -2,10 +2,15 @@ import time
 
 from driver import Driver
 
-def main():
-    with Driver() as driver:
-        driver.submit_guess("moist")
-        time.sleep(5)
+class SquarewordSolver:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def solve(self, first_guess="moist"):
+        self.driver.submit_guess(first_guess)
 
 if __name__ == '__main__':
-    main()
+    with Driver() as driver:
+        solver = SquarewordSolver(driver)
+        solved_data = solver.solve()
+        time.sleep(5)
